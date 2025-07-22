f7e7bd1bcba2d78f6fe1f727fb2d362d88b586c2
nikhilmandage <154799838+nikhilmandage@users.noreply.github.com>
2025-07-22 12:07:23 +0530

    Update app.py

--- microservices/user-service/app.py
+++ microservices/user-service/app.py
@@ -2,11 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def write_to_file():
-    with open('/data/message.txt', 'w') as f:
-        f.write("Hello from Order Service!")
-    return "Data written to /data/message.txt"
+def home():
+    return "User Service is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
